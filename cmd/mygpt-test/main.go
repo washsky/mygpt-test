abf8485819bfa5d1cb92f4c804a24588a51acfba
washsky <111639029+washsky@users.noreply.github.com>
2026-09-24 01:54:04 +0800

    feat: make server launch configurable and visible

--- cmd/mygpt-test/main.go
+++ cmd/mygpt-test/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/washsky/mygpt-test/internal/server"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+
+	"github.com/washsky/mygpt-test/internal/server"
+)
 
 var version = "dev"
 
 func main() {
-	server.Start(version)
+	addr := flag.String("addr", ":8080", "HTTP listen address")
+	showVersion := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("mygpt-test %s (%s)\n", version, runtime.Version())
+		return
+	}
+
+	if err := server.Start(*addr, version); err != nil {
+		log.Printf("server stopped: %v", err)
+		os.Exit(1)
+	}
 }
